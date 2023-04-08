8de6d66c48db069654e75c5788d4f74fef3a8fee
xiaohei <59282830+xiaoheibubu@users.noreply.github.com>
2023-04-08 18:45:41 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -102,9 +102,6 @@ function scrollActive(){
         sectionId = current.getAttribute('id')
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
     })
 }
