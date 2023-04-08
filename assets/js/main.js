0b9d38ba2b573c14650315f3b2a1836f17bf8b8c
xiaohei <59282830+xiaoheibubu@users.noreply.github.com>
2023-04-08 19:08:48 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -117,8 +117,6 @@ function scrollHeader(){
 /*==================== SHOW SCROLL UP ====================*/ 
 function scrollUp(){
     const scrollUp = document.getElementById('scroll-up');
-    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
-  }
   window.addEventListener('scroll', scrollUp)
 
 /*==================== DARK LIGHT THEME & LANGUAGE====================*/ 
